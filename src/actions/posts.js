b1bbb3ae86eae9200e0504b845eba62ab4025dc3
Gisele <gisesonia@gmail.com>
2019-03-09 22:32:52 -0300

    Removendo categories de post

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,11 +1,11 @@
 import uuid from "uuid";
-import getCategories from "../api";
 export const RECEIVE_POSTS = "RECEIVE_POSTS";
-export const RECEIVE_CATEGORIES = "RECEIVE_CATEGORIES";
 export const ADD_POST = "ADD_POST";
 export const EDIT_POST = "EDIT_POST";
 export const DELETE_POST = "DELETE_POST";
 export const SET_CATEGORY = "CATEGORY";
+
+
 
 export function receivePosts(posts) {
   return {
@@ -14,19 +14,6 @@ export function receivePosts(posts) {
   };
 }
 
-export function handleCategories() {
-  return dispatch => {
-    return getCategories().then(categories =>  dispatch(receiveCategories(categories)));
-  };
-} 
-
-
-function receiveCategories(category) {
-  return {
-    type: RECEIVE_CATEGORIES,
-    category
-  };
-} 
 
 export const addPost = ({ timestamp, title, body, author, category }) => ({
   type: ADD_POST,
