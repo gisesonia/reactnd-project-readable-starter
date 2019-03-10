c182b31c031d00ac7cbe7d09c001d4f7ecc68c56
Gisele <gisesonia@gmail.com>
2019-03-09 22:39:50 -0300

    Combinereducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import comments from "./comments";
 import posts from "./posts";
 import loading from "./loading";
+import categories from './categories';
 
 export default combineReducers({
   loading,
   posts,
-  comments
+  comments,
+ 
 });
