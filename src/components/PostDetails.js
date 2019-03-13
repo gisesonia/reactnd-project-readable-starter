5ff330407f262d3016a7116981e56759c59038f7
Gisele <gisesonia@gmail.com>
2019-03-12 22:30:53 -0300

    Props match params

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -1,46 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import Post from "./Post";
+import { handleInitialData } from "../actions/shared";
+import { handleComments } from "../actions/comments";
 import { MdAssignment } from "react-icons/md";
 
-const PostDetails = (props) => {
-  return (
-    <div>
-      <div className="pageTitle">
-        <h1>Posts</h1>
-        <MdAssignment className="icon-post" />
-      </div>
-      <div className="buttons">
-        <button
-          className=" btn order-date"
-          onClick={() => this.handleOptionChange("date")}
-        >
-          Order by date
-        </button>
-        <button
-          className=" btn order-score"
-          onClick={() => this.handleOptionChange("score")}
-        >
-          Order by Score
-        </button>
-      </div>
-      <ul className="postlist">
-        {this.props.posts.map(post => (
-          <li key={post.id}>
-            <Post post={post} />
-          </li>
-        ))}
-      </ul>
-      <button className="btn editar">Editar</button>
-      <button className="btn deletar">Deletar</button>
-    </div>
-  );
-};
+class PostDetails extends Component {
 
-function mapStateToProps({ posts }, {id}) {
+  constructor(props) {
+    super(props);
+    
+  }
+  componentDidMount() {
+    this.props.getInitialData();
+    this.props.getComments();
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="pageTitle">
+          <h1>Posts</h1>
+          <MdAssignment className="icon-post" />
+        </div>
+        <div className="buttons">
+          <button
+            className=" btn order-date"
+            onClick={() => this.handleOptionChange("date")}
+          >
+            Order by date
+          </button>
+          <button
+            className=" btn order-score"
+            onClick={() => this.handleOptionChange("score")}
+          >
+            Order by Score
+          </button>
+        </div>
+        <ul className="postlist">
+          <li />
+        </ul>
+        <button className="btn editar">Editar</button>
+        <button className="btn deletar">Deletar</button>
+      </div>
+    );
+  }
+}
+function mapStateToProps(state, {props}) {
+  const { postId } = props.match.params;
+  console.log(state);
   return {
-    posts: Object.values(posts)
+    postId
   };
 }
 
-export default connect(mapStateToProps)(PostDetails);
+const mapDispatchToProps = dispatch => ({
+  getComments() {
+    dispatch(handleComments());
+  },
+  getInitialData() {
+    dispatch(handleInitialData());
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PostDetails);
