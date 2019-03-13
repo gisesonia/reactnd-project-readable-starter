f99b5458c44a34f3e65dcd6bb4588c5a973c3139
Gisele <gisesonia@gmail.com>
2019-03-13 00:58:09 -0300

    criação de action de um post

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,7 +1,9 @@
 import uuid from "uuid";
-import APIaddposts from "../api"; 
-import APIdelPosts from "../api"; 
+import { APIaddposts } from "../api"; 
+import { APIfetchPost } from "../api"; 
+import {APIdelPosts} from "../api"; 
 export const RECEIVE_POSTS = "RECEIVE_POSTS";
+export const LOAD_POST = "LOAD_POST";
 export const ADD_POST = "ADD_POST";
 export const EDIT_POST = "EDIT_POST";
 export const DELETE_POST = "DELETE_POST";
@@ -11,7 +13,22 @@ export function receivePosts(posts) {
   return {
     type: RECEIVE_POSTS,
     posts
-  };
+  }
+}
+
+export function loadPost(post) {
+  return {
+    type: LOAD_POST,
+    post
+  }
+}
+
+export const fetchPost = (id) => {
+  return dispatch => {
+    return APIfetchPost(id).then(pid => {
+      dispatch(loadPost(pid))
+    })
+  }
 }
 
 export const deletePost = (id) => {
@@ -25,9 +42,9 @@ export const delpost = (idpost) => {
   return dispatch => {
     return APIdelPosts(idpost).then(idp => {
       dispatch(deletePost(idp));
-    });
-  };
-};
+    })
+  }
+}
 
 
 export const createpost = newpost => {
