159216b46a37892095aa3b67bfcf87be578b4d92
Gisele <gisesonia@gmail.com>
2019-03-20 21:59:21 -0300

    export duplicado

--- frontend/src/actions/posts.js
+++ frontend/src/actions/posts.js
@@ -2,13 +2,13 @@ import { v4 } from "uuid";
 import { APIaddposts } from "../api";
 import { APIdelPosts } from "../api";
 import { APIfetchPost } from "../api";
-export const RECEIVE_POSTS = "RECEIVE_POSTS";
 
+export const RECEIVE_POSTS = "RECEIVE_POSTS";
 export const FETCH_POST = "FETCH_POST";
 export const ADD_POST = "ADD_POST";
 export const EDIT_POST = "EDIT_POST";
 export const DELETE_POST = "DELETE_POST";
-export const FETCH_POST = "FETCH_POST";
+
 
 export function receivePosts(posts) {
   return {
